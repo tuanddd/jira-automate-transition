9c3d32df7ad45f560e23985f0c0433b6b163186d
Đào Tuấn <heryhudson@gmail.com>
2020-03-14 23:59:54 +0700

    Release v1.0.6

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,24 +1,26 @@
 import { JiraClient } from "./jira";
-import {
-  HandleChangesRequestedParams,
-  HandleMergedParams,
-  HandleReviewRequestedParams
-} from "./interfaces";
+import { HandleTransitionParams } from "./interfaces";
 
-const handleChangesRequested = async ({
+const handleTransitionIssue = async ({
   jiraTokenEncoded,
   jiraEndpoint,
   jiraIssueId,
-  columnToMoveToWhenChangesRequested
-}: HandleChangesRequestedParams) => {
+  colName
+}: HandleTransitionParams) => {
   const jira = new JiraClient(jiraTokenEncoded);
   const availableTransitions = await jira.request(
     `${jiraEndpoint}/rest/api/3/issue/${jiraIssueId}/transitions`
   );
   const transitionId = availableTransitions.transitions?.find(
-    (t: any) => t.to.name === columnToMoveToWhenChangesRequested
+    (t: any) => t.to.name === colName
   )?.id;
-  if (!transitionId) return;
+  if (!transitionId)
+    throw new Error(`
+    There was an error trying to transition issue ${jiraIssueId}:
+    Transition to status "${colName}" cannot be found.
+    Check all of the transitions' rules, conditions of yourr project's workflow.
+    See more: https://confluence.atlassian.com/adminjiraserver073/working-with-workflows-861253510.html
+  `);
   await jira.request(
     `${jiraEndpoint}/rest/api/3/issue/${jiraIssueId}/transitions`,
     "POST",
@@ -26,50 +28,4 @@ const handleChangesRequested = async ({
   );
 };
 
-const handleReviewRequested = async ({
-  jiraTokenEncoded,
-  jiraEndpoint,
-  columnToMoveToWhenReviewRequested,
-  jiraIssueId
-}: HandleReviewRequestedParams) => {
-  const jira = new JiraClient(jiraTokenEncoded);
-  const availableTransitions = await jira.request(
-    `${jiraEndpoint}/rest/api/3/issue/${jiraIssueId}/transitions`
-  );
-  const transitionId = availableTransitions.transitions?.find(
-    (t: any) => t.to.name === columnToMoveToWhenReviewRequested
-  )?.id;
-  if (!transitionId) return;
-  await jira.request(
-    `${jiraEndpoint}/rest/api/3/issue/${jiraIssueId}/transitions`,
-    "POST",
-    { transition: { id: transitionId } }
-  );
-};
-
-const handlePullRequestMerged = async ({
-  jiraTokenEncoded,
-  jiraEndpoint,
-  jiraIssueId,
-  columnToMoveToWhenMerged
-}: HandleMergedParams) => {
-  const jira = new JiraClient(jiraTokenEncoded);
-  const availableTransitions = await jira.request(
-    `${jiraEndpoint}/rest/api/3/issue/${jiraIssueId}/transitions`
-  );
-  const transitionId = availableTransitions.transitions?.find(
-    (t: any) => t.to.name === columnToMoveToWhenMerged
-  )?.id;
-  if (!transitionId) return;
-  await jira.request(
-    `${jiraEndpoint}/rest/api/3/issue/${jiraIssueId}/transitions`,
-    "POST",
-    { transition: { id: transitionId } }
-  );
-};
-
-export {
-  handleReviewRequested,
-  handleChangesRequested,
-  handlePullRequestMerged
-};
+export { handleTransitionIssue };
